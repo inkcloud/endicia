a3e9de60e2b2b7796d5796e6b0c29ef5e970627a
sarkistlt <sarkis.tlt@gmail.com>
2018-06-13 14:21:11 -0700

    default export resolve

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,1 +1,54 @@
-export * from './endicia';
+export declare type EndiciaMailClassType = 'PriorityExpress' | 'First' | 'LibraryMail' | 'MediaMail' | 'ParcelSelect' | 'RetailGround' | 'Priority' | 'PriorityMailExpressInternational' | 'FirstClassMailInternational' | 'FirstClassPackageInternationalService' | 'PriorityMailInternational';
+export declare enum EndiciaEndpoints {
+    test = "https://elstestserver.endicia.com/LabelService/EwsLabelService.asmx",
+    live = "https://labelserver.endicia.com/LabelService/EwsLabelService.asmx"
+}
+export declare enum EndiciaMailClass {
+    PriorityExpress = "PriorityExpress",
+    First = "First",
+    LibraryMail = "LibraryMail",
+    MediaMail = "MediaMail",
+    ParcelSelect = "ParcelSelect",
+    RetailGround = "RetailGround",
+    Priority = "Priority",
+    PriorityMailExpressInternational = "PriorityMailExpressInternational",
+    FirstClassMailInternational = "FirstClassMailInternational",
+    FirstClassPackageInternationalService = "FirstClassPackageInternationalService",
+    PriorityMailInternational = "PriorityMailInternational"
+}
+export interface EndiciaOptions {
+    mode: 'test' | 'live';
+    accountId: number;
+    passPhrase: string;
+    requesterId?: string;
+    label?: {
+        shipFrom?: ShippingAddress;
+    };
+}
+export interface ShippingAddress {
+    name: string;
+    careOf?: string;
+    address1: string;
+    address2?: string;
+    city: string;
+    stateProvince: string;
+    postalCode: string;
+    phone?: string;
+}
+export interface PostageLabelOption {
+    shipTo: ShippingAddress;
+    shipFrom?: ShippingAddress;
+    mailClass: EndiciaMailClassType;
+    weight: number;
+    mailPieceShape: string;
+    showReturnAddress?: boolean;
+    orderReference?: string;
+    customerReference?: string;
+}
+export interface ShipRateOption {
+    shipToPostalCode: string;
+    shipFromPostalCode?: string;
+    mailClass: string;
+    weight: number;
+    mailPieceShape: string;
+}
